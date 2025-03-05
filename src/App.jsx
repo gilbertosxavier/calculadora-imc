22cbd2e1a760884d1a6054f879c5f15e75f6dc42
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-03-05 16:23:16 -0300

    calculadora imc
    Calculadora de IMC.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './app.css';
 
 export default function App() {
 
-  const [peso,setPeso] = useState('');
-  const [altura, setAltura] = useState('');
+  const [weight,setWeight] = useState('');
+  const [height, setHeight] = useState('');
   const [message, setMessage] = useState('');
+  const [fadeOut, setFadOut] = useState(false);
 
-function calcularIMC () {
-  const alt = altura / 100;
-  const imc = peso / (alt*alt)
-
+function calculateIMC () {
+  const newHeight = height / 100;
+  const imc = weight / (newHeight * newHeight)
 
   if (imc < 18.6) {
    setMessage(`Você está abaixo do peso! Seu IMC: ${imc.toFixed(2)} `)
@@ -20,27 +20,32 @@ function calcularIMC () {
     setMessage(`Você está acima do peso! Seu IMC: ${imc.toFixed(2)}`)
   } else { setMessage(`Cuidado, Você está na faixa de obesidade! Seu IMC: ${imc.toFixed(2)}`)}
 
+  setWeight('');
+  setHeight('');
+  setFadOut(false);
+
+  setTimeout(() => setFadOut(true), 4000);
+  setTimeout(() => setMessage(""), 5000);
 
 }
 
 
   return (
     <div className="app">
-      <h1>Calculadora IMC</h1>
-      <span>Vamos calcular o seu IMC</span>
+      <h1>Calcule o seu IMC!</h1>
       <div className="area-input">
         <input 
         type="text"
         placeholder="Peso em (kg) ex: 80" 
-        value={peso}
-        onChange={ (e) => setPeso(e.target.value)}/>
+        value={weight}
+        onChange={ (e) => setWeight(e.target.value)}/>
         <input 
         type="text" 
         placeholder="Altura em (cm) ex: 180"
-        value={altura} 
-        onChange={(e) => setAltura(e.target.value)}/>
-      <button onClick={calcularIMC}>Calcular</button>
-      <h2>{message}</h2>
+        value={height} 
+        onChange={(e) => setHeight(e.target.value)}/>
+      <button onClick={calculateIMC}>Calcular</button>
+      {message && <h2 className={fadeOut ? "fade-out" : ""}>{message}</h2>}
       </div>
     </div>
 
